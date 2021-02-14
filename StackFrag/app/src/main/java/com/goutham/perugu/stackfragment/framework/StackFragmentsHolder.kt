f9bf1cc2a78751c0f4030ae59ef31138e2ebb46c
perugu.goutham <perugu.goutham@zohocorp.com>
2021-02-14 23:17:40 +0530

    House keeping - Removing reference for lib button theme

--- StackFrag/app/src/main/java/com/goutham/perugu/stackfragment/framework/StackFragmentsHolder.kt
+++ StackFrag/app/src/main/java/com/goutham/perugu/stackfragment/framework/StackFragmentsHolder.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.goutham.perugu.stackfragment.R
-import java.lang.RuntimeException
 
 class StackFragmentsHolder : Fragment(),
     BackPressListener,
